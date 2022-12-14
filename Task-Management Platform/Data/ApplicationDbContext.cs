f58641c5c3e8ad44d1c01cea2a4b7f1283d22486
Laurentiuq <gheorghe-laurentiu.ghetoiu@s.unibuc.ro>
2022-12-14 21:30:35 +0200

    migrations, collection instead of array for Task, new Database

--- Task-Management Platform/Data/ApplicationDbContext.cs
+++ Task-Management Platform/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using Task = Task_Management_Platform.Models.Task;
+//using Microsoft.AspNetCore.Identity;
 
 namespace Task_Management_Platform.Data
 {
@@ -16,29 +17,30 @@ namespace Task_Management_Platform.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Task> Tasks { get; set; }
         public DbSet<Team> Teams { get; set; }
- 
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+         
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
 
-            base.OnModelCreating(modelBuilder);
+        //    base.OnModelCreating(modelBuilder);
 
-            /* definire primary key compus
-            modelBuilder.Entity<ArticleBookmark>()
-                .HasKey(ab => new { ab.Id, ab.ArticleId, ab.BookmarkId });
+        //    /* definire primary key compus
+        //    modelBuilder.Entity<ArticleBookmark>()
+        //        .HasKey(ab => new { ab.Id, ab.ArticleId, ab.BookmarkId });
 
 
-            // definire relatii cu modelele Bookmark si Article (FK)
-            modelBuilder.Entity<ArticleBookmark>()
-                .HasOne(ab => ab.Article)
-                .WithMany(ab => ab.ArticleBookmarks)
-                .HasForeignKey(ab => ab.ArticleId);
+        //    // definire relatii cu modelele Bookmark si Article (FK)
+        //    modelBuilder.Entity<ArticleBookmark>()
+        //        .HasOne(ab => ab.Article)
+        //        .WithMany(ab => ab.ArticleBookmarks)
+        //        .HasForeignKey(ab => ab.ArticleId);
 
-            modelBuilder.Entity<ArticleBookmark>()
-                .HasOne(ab => ab.Bookmark)
-                .WithMany(ab => ab.ArticleBookmarks)
-                .HasForeignKey(ab => ab.BookmarkId); */
-        }
+        //    modelBuilder.Entity<ArticleBookmark>()
+        //        .HasOne(ab => ab.Bookmark)
+        //        .WithMany(ab => ab.ArticleBookmarks)
+        //        .HasForeignKey(ab => ab.BookmarkId); */
+        //} 
+        
     }
 }
--- Task-Management Platform/Program.cs
+++ Task-Management Platform/Program.cs
@@ -13,8 +13,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+  .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 builder.Services.AddControllersWithViews();
 
