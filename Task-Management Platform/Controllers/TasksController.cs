072fa2bbac797265fa96cabaa34ede2883467734
Laurentiuq <gheorghe-laurentiu.ghetoiu@s.unibuc.ro>
2022-12-18 03:31:43 +0200

    afisare paginata

--- Task-Management Platform/Controllers/TasksController.cs
+++ Task-Management Platform/Controllers/TasksController.cs
@@ -1,4 +1,6 @@
 ﻿using Humanizer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Task_Management_Platform.Data;
@@ -11,20 +13,94 @@ namespace Task_Management_Platform.Controllers
     {
         private readonly ApplicationDbContext db;
 
-        public TasksController(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public TasksController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
         {
             db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
         }
 
-
+        // Se afiseaza fiecare task si proiectul din care
+        // face parte si statusul
+        [Authorize(Roles = "User, Organizer, Admin")]
         public IActionResult Index()
         {
-            var tasks = db.Tasks;
-            ViewBag.Tasks = tasks;
+            var tasks = db.Tasks.OrderBy(a => a.DataStart);
+            var search = "";
+
+            //Motor de cautare
+
+            if (Convert.ToString(HttpContext.Request.Query["search"]) != null)
+            {
+                search = Convert.ToString(HttpContext.Request.Query["search"]).Trim();
+
+                List<int> taskIds = db.Tasks.Where
+                                        (
+                                            tsk => tsk.Title.Contains(search)
+                                            || tsk.Description.Contains(search)
+                                        ).Select(a => a.TaskId).ToList();
+                List<int> taskIdsOfCommentsWithSearchString = db.Comments
+                            .Where
+                            (
+                                c => c.Content.Contains(search)
+                            ).Select(c => (int)c.TaskId).ToList();
+                List<int> mergedIds = taskIds.Union(taskIdsOfCommentsWithSearchString).ToList();
+
+                tasks = db.Tasks.Where(task => mergedIds.Contains(task.TaskId))
+                                .Include("Status")
+                                .Include("User")
+                                .OrderBy(a => a.DataStart);
+            }
+
+            ViewBag.SearchString = search;
+
+
+            //Afisare paginata
 
             if (TempData.ContainsKey("message"))
             {
                 ViewBag.Message = TempData["message"];
+            }
+
+            int _perPage = 3;
+
+            if(TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+
+            int totalItems = tasks.Count();
+
+            var currentPage = Convert.ToInt32(HttpContext.Request.Query["page"]);
+            var offset = 0;
+
+            if(!currentPage.Equals(0))
+            {
+                offset = (currentPage - 1) * _perPage;
+            }
+
+            var paginatedTasks = tasks.Skip(offset).Take(_perPage);
+
+            ViewBag.lastPage = Math.Ceiling((float)totalItems / (float)_perPage);
+
+            ViewBag.Tasks = paginatedTasks;
+
+            if(search != "")
+            {
+                ViewBag.PaginationBaseUrl = "/Tasks/Index/?search=" + search + "&page";
+            }else
+            {
+                ViewBag.PaginationBaseUrl = "/Tasks/Index/?page";
             }
 
             return View();
---
+++ Task-Management Platform/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Task_Management_Platform.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Trebuie sa introduceti un nume pentru proiect")]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public virtual ApplicationUser Organizer { get; set; }
+        
+
+    }
+}
